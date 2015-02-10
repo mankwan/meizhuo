3ac6db1aba801e0e7d34527103e6a20101d29d37
mankwan <582604267@qq.com>
2015-02-10 23:59:49 +0800

    new

--- Public/Js/myjs.js
+++ Public/Js/myjs.js
@@ -1,8 +1,35 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
-				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-				anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+				sectionsColor: ['#1bbc9b', '#4BBFC3', '#C63D0F', '#7BAABE', '#1BBC9B','#ccddff'],
+				anchors: ['firstPage', 'secondPage', '3rdPage','4thPage', 'lastPage'],
 				menu: '#menu',
-				scrollingSpeed: 700
+				navigation: false,
+				scrollingSpeed: 700,
+	});
+	$('#group').hover(function(){
+		$(this).find('.design h1').animate({
+			left:'150px',
+			top:'150px',
+			fontSize:'10em',
+		},"slow");
+		$('.design p').animate({
+			left:'500px',
+			top:'300px',
+			fontSize:'4em',
+		},"slow");
+		
+		// $(this).find('.android h1').animate({
+		// 	right:'150px',
+		// 	top:'500px',
+		// 	fontSize:'10em',
+		// },"slow")
 	});
 });
+	// $('#work').fullpage({
+	// 	anchors: ['bozhi', 'e-tips', 'k'],
+	// 	Color: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+	// 	navigation:true,
+	// 	navigationPosition:'right',
+	// 	navigationTooltips: ['bozhi', 'e-tips', 'k'],
+	// 	responsive: 900,
+	// })();
